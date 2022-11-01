0b6a1ec39ea89d9bd919006cbf025e6018fbcab6
bus1hero <petru.cervac@gmail.com>
2022-11-01 22:19:38 +0200

    4.2 Refactor ParseChar to curried version

--- Program.cs
+++ Program.cs
@@ -1,21 +1,11 @@
-﻿// Firstly show this
-Console.WriteLine(ParseChar('A', "Amdaris"));
-Console.WriteLine(ParseChar('A', "Spam"));
-
-// Then show this
-Console.WriteLine(ParseChar('S', "Amdaris"));
-Console.WriteLine(ParseChar('S', "Spam"));
-
-
-// int -> int -> int
-var f1 = (int p1, int p2) => p1 * p2;
-var f2 = (int p1) => (int p2) => p1 * p2;
-Console.WriteLine(f1(6, 7));
-Console.WriteLine(f2(6)(7));
+﻿Console.WriteLine(ParseChar('A')("Amdaris"));
+Console.WriteLine(ParseChar('A')("Spam"));
+Console.WriteLine(ParseChar('S')("Amdaris"));
+Console.WriteLine(ParseChar('S')("Spam"));
 
 
 
-Result ParseChar(char ch, string input)
+Func<string, Result> ParseChar(char ch) => input =>
 {
 	if (string.IsNullOrEmpty(input))
 		return new Failure("input is null or empty");
