c2b2b3cf0377fec77ecbe5c170bcb2a9caf34869
bus1hero <petru.cervac@gmail.com>
2022-11-01 22:14:51 +0200

    Parse a specified character

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
-﻿Console.WriteLine(ParseA("Amdaris"));
-Console.WriteLine(ParseA("Spam"));
+﻿// Firstly show this
+Console.WriteLine(ParseChar('A', "Amdaris"));
+Console.WriteLine(ParseChar('A', "Spam"));
 
-(bool, string) ParseA(string input)
+// Then show this
+Console.WriteLine(ParseChar('S', "Amdaris"));
+Console.WriteLine(ParseChar('S', "Spam"));
+
+
+(bool, string) ParseChar(char ch, string input)
 {
 	if (string.IsNullOrEmpty(input))
 		return (false, input);
-	if (input[0] != 'A')
+	if (input[0] != ch)
 		return (false, input);
 	return (true, input[1..]);
-}
+};
